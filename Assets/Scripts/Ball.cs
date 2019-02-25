a9456350487d201fbe34f86f4e301e62aa5d89de
Eben Blaisdell <emblaisdell@foresthills.net>
2019-02-25 09:38:47 -0500

    remove unnecessary balls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    const float FLOOR_POS = -10f;
+
+    void Update()
+    {
+        if (transform.position.y < FLOOR_POS)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
